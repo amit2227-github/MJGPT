b100af3b2a1cc4e21c7c9c7cf92fc41045df8625
Amit Jirobe <amitjirobe07@gmail.com>
2026-02-03 23:17:01 +0530

    Initial project setup

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,23 +3,21 @@ import Sidebar from './components/Sidebar'
 import { Route, Routes } from 'react-router-dom'
 import ChatBox from './components/ChatBox'
 import Community from './pages/Community'
+import Credits from './pages/Credits'
 
 const App = () => {
-  return(
-    <> 
-
-    <div className='dark:bg-gardient-to-b from-[#242124] to-[#000000] dark:text-white'>
-      <div className='flex h-screen w-screen' >
-        <Sidebar/>
-        <Routes>
-          <Route path='/' element={<ChatBox/>}/>
-          <Route path='/credits' element={<Credits/>}/>
-          <Route path='/community' element={<Community/>}/>
-        </Routes>
+  return (
+    <>
+      <div className='dark:bg-gradient-to-b from-[#242124] to-[#000000] dark:text-white'>
+        <div className='flex h-screen w-screen'>
+          <Sidebar />
+          <Routes>
+            <Route path='/' element={<ChatBox />} />
+            <Route path='/credits' element={<Credits />} />
+            <Route path='/community' element={<Community />} />
+          </Routes>
+        </div>
       </div>
-    </div>
-      
-
     </>
   )
 }
